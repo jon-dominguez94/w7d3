868ca47fe6e86d7ae856af1a2bd38d1b7eba450d
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-12-05 14:47:28 -0800

    Implement gifts

--- skeleton/config/routes.rb
+++ skeleton/config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
     resources :guests, only: [:index, :show] do
       resources :gifts, only: [:index]
     end
-  end
 
     resources :gifts, only: [:show]
     resources :parties, only: [:index, :show]
+  end
 end
