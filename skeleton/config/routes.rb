ef80ed7b57d0cbef2f9f1ef593a4891ba6ec7117
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-12-05 13:51:11 -0800

    Create guest show view page

--- skeleton/config/routes.rb
+++ skeleton/config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
-    resources :guests only: [:index, :show] do
-      resources :gifts only: [:index, :show]
+    resources :guests, only: [:index, :show] do
+      resources :gifts, only: [:index]
     end
+  end
 
-    resources :parties only: [:index, :show]
-end
-
-
+    resources :gifts, only: [:show]
+    resources :parties, only: [:index, :show]
 end
