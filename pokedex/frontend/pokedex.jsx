f1e82b390a7607c011baaa313699f6c703c35676
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-12-05 17:17:01 -0800

    Create selector

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
 import { fetchAllPokemon } from './util/api_util';
 import configureStore from './store/store';
+import { selectAllPokemon } from './reducers/selectors';
 
 
 window.receiveAllPokemon = receiveAllPokemon;
 window.requestAllPokemon = requestAllPokemon;
 window.fetchAllPokemon = fetchAllPokemon;
+window.selectAllPokemon = selectAllPokemon;
 
 const store = configureStore();
 window.store = store;
